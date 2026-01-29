18b0f78032fe8a7969dbaab83b19afd5e7ae56e0
Waqas Raza <wqsrza07@gmail.com>
2026-01-29 18:20:12 +1100

    new changes

--- server.js
+++ server.js
@@ -1,34 +1,38 @@
 const express = require('express')
-
 const app = express()
 
 app.use(express.json())
 
 let notes = []
 
-app.get('/' , (req , res)=>{
+app.get('/', (req , res)=>{
   res.send(notes)
 })
 
-app.post('/' , (req , res)=>{
+app.post('/' , (req , res)=> {
    notes.push(req.body)
+   res.send(notes)
+   
+  })
+
+app.delete('/:id' , (req , res)=>{
+  delete notes[req.params.id]
    res.send(notes)
 })
 
-app.delete('/notes/:index' , (req , res)=> {
-  delete notes[req.params.id]
-  res.send(notes)
+app.patch('/:id', (req, res) => {
+  const id = Number(req.params.id)
 
-  console.log(req.body);
-  
-})
+  notes[id] = {
+    ...notes[id],
+    ...req.body
+  }
 
-app.patch('/:id' , (req , res)=>{
-  notes[req.params.id].description = req.body.description
   res.send(notes)
 })
 
-app.listen(3000 , ()=>{
-  console.log('server is running on port 3000');
+
+app.listen(3000  ,()=>{
+  console.log("server is running on port 3000 ")
   
 })
