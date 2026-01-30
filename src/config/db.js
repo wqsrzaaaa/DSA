53adcf57be3bf10c124d7dd253903adbabbe6e37
Waqas Raza <wqsrza07@gmail.com>
2026-01-30 22:17:55 +1100

    new cahnges

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
 
 export function db() {
-  console.log("Mongo URI:", process.env.MONGODB); // DEBUG
-
   mongoose
     .connect(process.env.MONGODB)
     .then(() => console.log("db connected"))
-    .catch(err => console.error(err))
 }
